199e909113170661bf6b64739db3c7075584a0b9
Julien GOURDET <julien.gourdet.ext@francetv.fr>
2017-02-22 11:36:21 +0100

    add SwiftMonkey.xcscheme for Carthage support

---
+++ SwiftMonkey/SwiftMonkey-Swift.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMonkey-Swift.h
+//  SwiftMonkey-Swift
+//
+//  Created by Julien GOURDET on 21/02/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMonkey.
+FOUNDATION_EXPORT double SwiftMonkeyVersionNumber;
+
+//! Project version string for SwiftMonkey.
+FOUNDATION_EXPORT const unsigned char SwiftMonkeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMonkey/PublicHeader.h>
+
+
