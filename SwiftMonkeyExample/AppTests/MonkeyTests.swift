c99583ce8c06abf9730ca1c3a879d6d27b9d668d
Dag Ågren <dag.agren@zalando.fi>
2017-02-28 11:49:38 +0200

    Testing using public XCTest API to direct taps to randomly selected elements.

--- SwiftMonkeyExample/AppTests/MonkeyTests.swift
+++ SwiftMonkeyExample/AppTests/MonkeyTests.swift
@@ -41,8 +41,51 @@ class SwiftMonkeyExampleUITests: XCTestCase {
         // Use either one of these but maybe not both.
         // XCTest private actions seem to work better at the moment.
         // UIAutomation actions seem to work only on the simulator.
-        monkey.addDefaultXCTestPrivateActions()
+        //monkey.addDefaultXCTestPrivateActions()
         //monkey.addDefaultUIAutomationActions()
+
+        monkey.addAction(weight: 1) {
+            let types: Set<XCUIElementType> = [
+                .button,
+                .statusBar,
+                .slider,
+                .pageIndicator,
+                .segmentedControl,
+                .switch,
+                .toggle,
+                //.link,
+                //.searchField,
+                //.scrollView,
+                .scrollBar,
+                //.textField,
+                //.secureTextField,
+                //.textView,
+                .map,
+                //.webView,
+                .incrementArrow,
+                .decrementArrow,
+                .ratingIndicator,
+                .cell,
+                .stepper,
+            ]
+
+            let descendants = application.descendants(matching: .any)
+            let count = descendants.count
+            var elements: [XCUIElement] = []
+
+            for index in 0 ..< count {
+                let element = descendants.element(boundBy: index)
+                if types.contains(element.elementType) {
+                    elements.append(element)
+                }
+            }
+
+            let index = monkey.randomInt(lessThan: elements.count)
+            let element = elements[index]
+            let randomOffset = CGVector(dx: monkey.randomCGFloat(), dy:  monkey.randomCGFloat())
+            let coordinate = element.coordinate(withNormalizedOffset: randomOffset)
+            coordinate.tap()
+        }
 
         // Occasionally, use the regular XCTest functionality
         // to check if an alert is shown, and click a random
