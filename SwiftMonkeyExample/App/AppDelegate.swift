87c16c428f2f361527defc4c23779324bcf665d0
Wojciech Czerski <wojciech.czerski@gmail.com>
2018-09-23 15:24:33 +0200

    Convert the Example project to Swift 4.2

--- SwiftMonkeyExample/App/AppDelegate.swift
+++ SwiftMonkeyExample/App/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var paws: MonkeyPaws?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         paws = MonkeyPaws(view: window!)
         return true
     }
